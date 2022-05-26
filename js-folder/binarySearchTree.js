9a6fc4b7f4075ec1433259b7c9bf5461ac3233a3
jacobnathanson <jacobnath93@gmail.com>
2022-05-25 21:15:13 -0400

    remove method

--- js-folder/binarySearchTree.js
+++ js-folder/binarySearchTree.js
@@ -11,7 +11,6 @@ class BinarySearchTree {
         this.root = null;
     }
     insert(value) {
-
         const newNode = new Node(value);
 
         if (!this.root) {
@@ -60,8 +59,127 @@ class BinarySearchTree {
         // did not find
         return null;
     }
-    // remove
+    // return target node and previousNode
+    lookUpTarget(value) {
+        let currentNode = this.root;
+        let previousNode = null;
+        let direction = '';
+
+        while (currentNode !== null) {
+            if (value === currentNode.value) {
+                return {
+                    target: currentNode,
+                    targetPredecessor: previousNode,
+                    direction
+                }
+            }
+            //update previousNode
+            previousNode = currentNode;
+
+            // less than, move to left node
+            if (value < currentNode.value) {
+                direction = 'left';
+                currentNode = currentNode.left;
+                // more than, move to right node
+            } else {
+                direction = 'right';
+                currentNode = currentNode.right;
+            }
+        }
+        // did not find
+        return null;
+    }
+    remove(value) {
+        const lookup = this.lookUpTarget(value);
+
+        if (!lookup) {
+            return 'not found';
+        }
+        let { target, targetPredecessor, direction } = lookup;
+
+        // target has no children
+        if (!target.left && !target.right) {
+            // make root target null
+            if (target === this.root) {
+                this.root = null;
+            } else {
+                targetPredecessor[direction] = null;
+            }
+            return this;
+        }
+
+        // target only has one left direction child
+        if (target.left && !target.right) {
+            // update root
+            if (target === this.root) {
+                this.root = target.left;
+            } else {
+                targetPredecessor[direction] = target.left;
+            }
+        }
+        // target only has one right direction child
+        else if (!target.left && target.right) {
+            // update root
+            if (target === this.root) {
+                this.root = target.right;
+            } else {
+                targetPredecessor[direction] = target.right;
+            }
+        }
+        // target has 2 children
+        else {
+            let successor = target.right;
+            let preSuccessor = successor;
+
+            // while not on the last left most node
+            while (successor.left) {
+                preSuccessor = successor;
+                successor = successor.left;
+            }
+
+            // remove successor and child from the tree
+            if (successor.right) {
+                preSuccessor.left = successor.right;
+                successor.right = null;
+            } else {
+                preSuccessor.left = null;
+            }
+
+            // grab left side of the target
+            const leftSide = target.left;
+            // assign to successor
+            successor.left = leftSide;
+
+            // check if root
+            if (target === this.root) {
+                this.root = successor;
+            } else {
+                // grab right side of target and assign to successor
+                const rightSide = target.right;
+                successor.right = rightSide;
+                targetPredecessor[direction] = successor;
+            }
+        }
+        return this;
+    }
 }
+
+const tree = new BinarySearchTree();
+tree.insert(25)
+tree.insert(1)
+tree.insert(50)
+// console.log(tree.remove(25))
+tree.insert(26)
+tree.insert(51)
+// tree.insert(51)
+// tree.remove(25)
+// tree.insert(6)
+// tree.insert(20)
+// tree.insert(170)
+// tree.insert(15)
+// tree.insert(1)
+// tree.remove(9)
+// console.log(tree)
 
 // ZTM solution
 class BinarySearchTree2 {
@@ -94,10 +212,6 @@ class BinarySearchTree2 {
                     currentNode = currentNode.right;
                 }
             }
-
-
-
-
         }
 
     }
@@ -120,17 +234,96 @@ class BinarySearchTree2 {
         }
         return false;
     }
+
+
+    remove(value) {
+        if (!this.root) {
+            return false;
+        }
+        let currentNode = this.root;
+        let parentNode = null; //previous reference to parent
+
+        // search for node based on value
+        while (currentNode) {
+            if (value < currentNode.value) {
+                // update parent node basied on direction
+                parentNode = currentNode;
+                currentNode = currentNode.left;
+            } else if (value > currentNode.value) {
+                parentNode = currentNode;
+                currentNode = currentNode.right;
+            } else if (currentNode.value === value) {
+                //We have a match, get to work! parent node is set
+
+                //Option 1: No right child: 
+                //    9
+                // 5
+                if (currentNode.right === null) {
+                    // if parent node is still null then we are at the root
+                    if (parentNode === null) {
+                        this.root = currentNode.left;
+                    } else {
+                        //if parent > current value, make current left child a child of parent
+                        if (currentNode.value < parentNode.value) {
+                            parentNode.left = currentNode.left;
+
+                            //if parent < current value, make left child a right child of parent
+                        } else if (currentNode.value > parentNode.value) {
+                            parentNode.right = currentNode.left;
+                        }
+                    }
+
+                    //Option 2: Right child which doesnt have a left child
+                } else if (currentNode.right.left === null) {
+                    currentNode.right.left = currentNode.left;
+                    if (parentNode === null) {
+                        this.root = currentNode.right;
+                    } else {
+
+                        //if parent > current, make right child of the left the parent
+                        if (currentNode.value < parentNode.value) {
+                            parentNode.left = currentNode.right;
+
+                            //if parent < current, make right child a right child of the parent
+                        } else if (currentNode.value > parentNode.value) {
+                            parentNode.right = currentNode.right;
+                        }
+                    }
+
+                    //Option 3: Right child that has a left child
+                } else {
+
+                    //find the Right child's left most child
+                    let leftmost = currentNode.right.left;
+                    let leftmostParent = currentNode.right;
+                    while (leftmost.left !== null) {
+                        leftmostParent = leftmost;
+                        leftmost = leftmost.left;
+                    }
+
+                    //Parent's left subtree is now leftmost's right subtree
+                    leftmostParent.left = leftmost.right;
+                    leftmost.left = currentNode.left;
+                    leftmost.right = currentNode.right;
+
+                    if (parentNode === null) {
+                        this.root = leftmost;
+                    } else {
+                        if (currentNode.value < parentNode.value) {
+                            parentNode.left = leftmost;
+                        } else if (currentNode.value > parentNode.value) {
+                            parentNode.right = leftmost;
+                        }
+                    }
+                }
+                return true;
+            }
+        }
+    }
+
+
 }
 
-const tree = new BinarySearchTree();
-tree.insert(9)
-tree.insert(4)
-tree.insert(6)
-tree.insert(20)
-tree.insert(170)
-tree.insert(15)
-tree.insert(1)
-console.log(tree)
 
 // console.log(tree.lookup(15))
 // JSON.stringify(traverse(tree.root))
