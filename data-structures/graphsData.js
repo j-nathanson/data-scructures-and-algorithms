4b31f60ac48828df415b9e149bc77d56b4ae3060
jacobnathanson <jacobnath93@gmail.com>
2022-05-27 13:11:43 -0400

    renamed folder added graph DS

--- data-structures/graphsData.js
+++ data-structures/graphsData.js
@@ -4,7 +4,7 @@ const graph = [[0, 2], [2, 3][2, 1], [1, 3]];
 
 // Adjacent List
 // index is the node, value is nodes neighbors/connections. arrays, objects or linked list
-const graph2 = [2, [2, 3], [0, 1, 3], [1, 2]];
+const graph2 = [[2], [2, 3], [0, 1, 3], [1, 2]];
 
 // Adjacent Matrix
 
